d14c1ee625777e93a0188e4a2d852403b4582177
zra <114052543+Ezrafi@users.noreply.github.com>
2025-09-02 20:29:19 +0700

    Update layout.php
    add footer

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -8,16 +8,21 @@
     <style>
         body {
             display: flex;
+            flex-direction: column;
             min-height: 100vh;
             margin: 0;
         }
-        .sidebar {
+        .layout {
+            flex: 1;
+            display: flex;
+        }
+        aside.sidebar {
             width: 220px;
             background-color: #212529;
             color: #fff;
             padding: 20px 10px;
         }
-        .sidebar a {
+        aside.sidebar a {
             color: #fff;
             text-decoration: none;
             display: block;
@@ -25,10 +30,10 @@
             margin: 5px 0;
             border-radius: 5px;
         }
-        .sidebar a:hover {
+        aside.sidebar a:hover {
             background-color: #495057;
         }
-        .content {
+        main.content {
             flex: 1;
             padding: 20px;
             background-color: #f8f9fa;
@@ -37,22 +42,36 @@
 </head>
 <body>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <h4 class="text-white mb-4">Sistem Gudang</h4>
-        <a href="/">🏠 Dashboard</a>
-        <a href="/kategori">📂 Kategori</a>
-        <a href="/barang">📦 Barang</a>
-        <a href="/pembelian">🧾 Data Pembelian</a>
-        <a href="/barangmasuk">⬆️ Barang Masuk</a>
-        <a href="/barangkeluar">⬇️ Barang Keluar</a>
+    <!-- Layout -->
+    <div class="layout">
+        <!-- Sidebar -->
+        <aside class="sidebar">
+            <h4 class="mb-4">Sistem Gudang</h4>
+            <a href="/">🏠 Dashboard</a>
+            <a href="/kategori">📂 Kategori</a>
+            <a href="/barang">📦 Barang</a>
+            <a href="/pembelian">🧾 Data Pembelian</a>
+            <a href="/barangmasuk">⬆️ Barang Masuk</a>
+            <a href="/barangkeluar">⬇️ Barang Keluar</a>
+        </aside>
+
+        <!-- Main Content -->
+        <main class="content">
+            <?= $this->renderSection('content') ?>
+        </main>
     </div>
 
-    <!-- Content -->
-    <div class="content">
-        <?= $this->renderSection('content') ?>
-    </div>
+    <!-- Footer -->
+    <footer class="py-1 border-top mt-auto bg-light">
+        <div class="container text-center">
+            <ul class="nav justify-content-center mb-1">
+                
+            </ul>
+            <p class="text-body-secondary mb-0">&copy; Atha Ezrafi</p>
+        </div>
+    </footer>
+    
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
